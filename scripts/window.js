ecc8b22b84e2f2a33738e19263c0a990b2649d14
Nicole De Luca <ddeluca@oberlin.edu>
2021-12-22 15:42:29 -0800

    Profile functionality
    Added basic profile functionality. Profiles store lists of tabs and can be switched through using the drop-down menu. Working tabs update on switch. Profiles can be deleted using the delete button, which asks for confirmation first. On backend, profiles are stored in profile_dict which is updated on start as well as on profile creation.

--- scripts/window.js
+++ scripts/window.js
@@ -3,13 +3,19 @@
 var storeButton = document.querySelector('.store');
 var openButton = document.querySelector('.open');
 var clearButton = document.querySelector('.clear');
+var saveButton = document.querySelector('.save');
+var deleteButton = document.querySelector('.delete');
 
 storeButton.addEventListener('click', storeTabs);
 openButton.addEventListener('click', openTabs);
 clearButton.addEventListener('click', clearTabs);
+saveButton.addEventListener('click', saveProfile);
+deleteButton.addEventListener('click', deleteConfirm);
 
-var tab_dict = {}; // Will probably have to use an initialization function to load previously stored tabs eventually
+var profile_dict = {}; // Will probably have to use an initialization function to load previously stored tabs eventually
 var working_tabs = [];
+
+document.getElementById('tab-profiles').onchange = profileSelect; // Couldn't get this to work in the HTML for some reason so we're setting it here
 
 // Grab local storage to initialize the working tab array.
 // Runs the companion function listWorkingTabs() upon completion
@@ -33,6 +39,21 @@ function initialize_working_tabs() {
   });
 }
 
+// Grab local storage to initialize profile storage.
+// TODO: create a "default" profile that contains all open tabs?
+function initialize_profiles() {
+  var getStorage = browser.storage.local.get(null);
+  getStorage.then((results) => {
+    for(let dict in results) {
+      if(dict != "working") {
+        profile_dict[dict] = results[dict];
+      }
+    }
+    console.log("Profiles initialized successfully.");
+  });
+}
+
+// On button click functions.
 function storeTabs() {
   var storing = browser.storage.local.set({ working: working_tabs });
   console.log("Your working tabs have been stored.");
@@ -43,9 +64,63 @@ function openTabs() {
 }
 
 function clearTabs() {
-  browser.storage.local.clear();
+  var getStorage = browser.storage.local.get(null);
+  getStorage.then((results) => {
+    delete results["working"];
+    browser.storage.local.clear(); // This maybe isn't best practice... could lose all data if something goes wrong
+    for(let profile in profile_dict) {
+      browser.storage.local.set({ [profile]: profile_dict[profile] });
+    }
+  });
   working_tabs = []; // Do I really want this?
+  listWorkingTabs();
   console.log("Stored working tabs have been cleared.");
+}
+
+function saveProfile() {
+  var textBox = document.getElementById('profile-name');
+  var storing = browser.storage.local.set({ [textBox.value]: working_tabs });
+  storing.then(() => { updateProfiles(); });
+  initialize_profiles();
+  console.log("Profile saved.");
+}
+
+// First called by button to confirm deletion of profile.
+// Swaps event listeners to actual delete function and
+// edits text to confirm deletion of profile with user.
+function deleteConfirm() {
+  document.getElementById('delete-button').innerText = "Are you sure?";
+  deleteButton.removeEventListener('click', deleteConfirm)
+  deleteButton.addEventListener('click', deleteProfile);
+}
+
+// Actual delete function.
+// Swaps event listeners back and edits text upon completion of delete.
+function deleteProfile() {
+  var profileName = document.getElementById('tab-profiles').value;
+  delete profile_dict[profileName];
+  browser.storage.local.clear(); // This maybe isn't best practice... could lose all data if something goes wrong
+  for(let profile in profile_dict) {
+    browser.storage.local.set({ [profile]: profile_dict[profile] });
+  }
+  browser.storage.local.set({ working: working_tabs }); // Might not need in the future?
+
+  document.getElementById('delete-button').innerText = "Delete Profile";
+  deleteButton.removeEventListener('click', deleteProfile);
+  deleteButton.addEventListener('click', deleteConfirm);
+
+  console.log("Profile deleted.")
+  updateProfiles();
+  listWorkingTabs();
+}
+
+// Update working tabs (and list) when profile is selected.
+// TODO: doesn't work if only one profile exists. Might not need
+// to worry about this if we have a "default" profile in the future
+function profileSelect() {
+  var profileName = document.getElementById('tab-profiles').value;
+  working_tabs = profile_dict[profileName];
+  listWorkingTabs();
 }
 
 // Helper function
@@ -54,6 +129,7 @@ function getCurrentWindowTabs() {
 }
 
 // List tabs from current active window. Hyperlinks contain href, url, and title information.
+// Will need to change to replaceChildren() structure if list requires being updated.
 function listTabs() {
   getCurrentWindowTabs().then((tabs) => {
     let tabsList = document.getElementById('list-tabs');
@@ -81,7 +157,7 @@ function listTabs() {
 function listWorkingTabs() {
   let workingTabsList = document.getElementById('working-tabs');
   let bufTabs = document.createDocumentFragment();
-  bufTabs.textContent = "Working tabs: "
+  bufTabs.textContent = "Working tabs: ";
   bufTabs.appendChild(document.createElement('br'));
 
   for(let tab of working_tabs) {
@@ -101,23 +177,23 @@ function listWorkingTabs() {
 // Will probably be called on creation of new profiles by user
 // in order to keep display up to date.
 function updateProfiles() {
-  var getStorage = browser.storage.local.get(null)
+  var getStorage = browser.storage.local.get(null);
   getStorage.then((results) => {
     var keys = Object.keys(results);
     var dropdownMenu = document.getElementById('tab-profiles'); // HTMLOptionElement for the drop-down menu
+    let bufMenu = document.createDocumentFragment();
     var profile, buf; // Working variables for usage with HTML elements
-    //console.log("Parent element: ");
-    //console.log(dropdownMenu)
 
     // Loop through all keys to add profiles to drop-down menu
     for(let key of keys) {
       if(key != "working") { // We don't want to add the working tabs as a profile
         profile = document.createElement('option');
-        buf = dropdownMenu.appendChild(profile);
-        buf.value = key;
-        buf.text = key;
+        profile.value = key;
+        profile.text = key;
+        bufMenu.appendChild(profile);
       }
     }
+    dropdownMenu.replaceChildren(bufMenu);
   });
 }
 
@@ -125,6 +201,7 @@ function updateProfiles() {
 document.addEventListener("DOMContentLoaded", initialize_working_tabs);
 document.addEventListener("DOMContentLoaded", listTabs);
 document.addEventListener("DOMContentLoaded", updateProfiles);
+document.addEventListener("DOMContentLoaded", initialize_profiles);
 
 document.addEventListener("click", (e) => {
   if(e.target.classList.contains('tab-click')) {
